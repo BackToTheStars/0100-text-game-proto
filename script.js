3ea7a87f99aaad3dcd0a1dc80278f1d9e71d7fd8
BackToTheStars <nikgrigoryev@gmail.com>
2020-07-26 16:53:16 -0700

    Lesson 1

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
 // todo: 1) Typescript
 
-
 let gameBox = document.getElementById("gameBox");
 
-const getNewHeaderInput = () => {
-  let input = document.getElementById("headerText");
-  return input.value;
+function getInputValue(id) {
+  let input = document.getElementById(id);
+  let text = input.value;
+  input.value = "";
+  return text;
 }
 
-const getNewParagraphInput = () => {
-  let input = document.getElementById("paragraphText");
-  return input.value;
-}
-
-const makeNewBoxMessage = (headStr, parStr) => {
+function makeNewBoxMessage(headStr, parStr) {
   let div = document.createElement("div");
   div.className = "textBox";
-  div.innerHTML = "<h1>" + headStr + "</h1><hr><p>" + parStr + "</p>";
+  div.innerHTML = "<h1 class='headerText'>" + headStr + "</h1><hr><p class='paragraphText'>" + parStr + "</p>";
   return div;
 }
 
-const addNewBoxToGame = () => {
-  let header = getNewHeaderInput();
-  let par = getNewParagraphInput();
+function addNewBoxTOGame() {
+  let header = getInputValue("headerText");
+  let par = getInputValue("paragraphText");
   let newDiv = makeNewBoxMessage(header, par);
   gameBox.appendChild(newDiv);
+}
+
+function insertNewClassElement(input, ul) {
+  let value = input.value;
+  input.value = "";
+  let li = document.createElement("li");
+  li.innerHTML = value;
+  ul.appendChild(li);
+}
+
+function createClassField(name) {
+  let uniqueInputId = "classInput" + name;
+  let uniqueUlId = "classUl" + name;
+  let div = document.createElement("div");
+  div.className = "row";
+  div.innerHTML = "<h3>" + name + "</h3>" +
+    "<ul id='" + uniqueUlId + "'></ul>" +
+    "<input id='" + uniqueInputId + "'> " +
+    "<button onclick='insertNewClassElement(" + uniqueInputId + "," + uniqueUlId + ")'>add</button>";
+  return div;
+}
+
+function insertNewClass(childClass) {
+  let parent = document.getElementById("classMenu");
+  parent.appendChild(childClass);
+}
+
+function addNewClass() {
+  let newClassName = getInputValue("newClassName");
+  let newClassDiv = createClassField(newClassName);
+  insertNewClass(newClassDiv);
 }
 
 
