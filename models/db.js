14366a4b2dc2f9731f7476798bd411c7ca4658e4
BackToTheStars <nikgrigoryev@gmail.com>
2020-08-24 21:34:37 -0700

    Modal window saves turn + external database

--- models/db.js
+++ models/db.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
-const MONGO_URL = "mongodb://127.0.0.1:27017/TextGame"
+const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/TextGame"
 mongoose.connect(MONGO_URL, {useUnifiedTopology: true, useNewUrlParser: true});
