a6a73b0b8a23ee369c52b3d7cc03e2bcc2b83fc6
BackToTheStars <nikgrigoryev@gmail.com>
2021-01-13 21:11:30 -0800

    minor

--- server/scripts/db_dump.js
+++ server/scripts/db_dump.js
@@ -1,20 +1,21 @@
-const { resolve } = require("path");
-const fs = require("fs");
-const { exec } = require("child_process");
-require("dotenv").config({
-    path: resolve(__dirname, "../.env"),
+const { resolve } = require('path');
+const fs = require('fs');
+const { exec } = require('child_process');
+require('dotenv').config({
+    path: resolve(__dirname, '../.env'),
 });
 
-const dumpsDir = resolve(__dirname, "../dumps");
+const dumpsDir = resolve(__dirname, '../dumps');
 const date = new Date();
-let folderName = `${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(
+let folderName = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(
     -2
-)}-${("0" + date.getDate()).slice(-2)}-`;
+)}-${('0' + date.getDate()).slice(-2)}-`;
 
 const files = fs.readdirSync(dumpsDir);
 for (let i = 1; i < files.length + 2; i++) {
     if (files.indexOf(`${folderName}${i}`) === -1) {
         folderName = `${folderName}${i}`;
+
         break;
     }
 }
