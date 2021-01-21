9d3c4131643efdd250afabdaa56532c693d226ae
BackToTheStars <nikgrigoryev@gmail.com>
2021-01-20 22:46:55 -0800

    fixed left classes panel

--- server/controllers/turns.js
+++ server/controllers/turns.js
@@ -4,119 +4,119 @@ const bunyan = require('bunyan');
 const log = bunyan.createLogger({ name: 'turns', level: 'info' });
 
 async function updateTurn(req, res) {
-    log.debug(`Entering ... ${arguments.callee.name}`);
-    const { id } = req.params;
-    const { gameId } = req.gameInfo;
-    const turn = req.body;
-    const turnModel = await Turn.findOneAndUpdate(
-        {
-            gameId,
-            _id: id,
-        },
-        {
-            ...turn,
-            _id: id,
-            gameId,
-        },
-        { new: true }
-    ); //функция ищет по ид и апдейтит
-    // log.debug(`Ending ... ${arguments.callee.name}`);
-    res.json({
-        item: turnModel,
-    }); // new true говорит отдать новую модель, а не старую
-    await screenshooter.getScreenshot(); // selenium снимок экрана
+  log.debug(`Entering ... ${arguments.callee.name}`);
+  const { id } = req.params;
+  const { gameId } = req.gameInfo;
+  const turn = req.body;
+  const turnModel = await Turn.findOneAndUpdate(
+    {
+      gameId,
+      _id: id,
+    },
+    {
+      ...turn,
+      _id: id,
+      gameId,
+    },
+    { new: true }
+  ); //функция ищет по ид и апдейтит
+  // log.debug(`Ending ... ${arguments.callee.name}`);
+  res.json({
+    item: turnModel,
+  }); // new true говорит отдать новую модель, а не старую
+  // await screenshooter.getScreenshot(); // selenium снимок экрана
 }
 
 async function deleteTurn(req, res) {
-    log.debug(`Entering ... ${arguments.callee.name}`);
-    const { gameId } = req.gameInfo;
-    const { id } = req.params;
-    const turnModel = await Turn.findOneAndRemove({
-        _id: id,
-        gameId,
-    }); //функция ищет по ид и удаляет
-    // log.debug(`Ending ... ${arguments.callee.name}`);
-    res.json({
-        item: turnModel,
-    }); // new true говорит отдать новую модель, а не старую
+  log.debug(`Entering ... ${arguments.callee.name}`);
+  const { gameId } = req.gameInfo;
+  const { id } = req.params;
+  const turnModel = await Turn.findOneAndRemove({
+    _id: id,
+    gameId,
+  }); //функция ищет по ид и удаляет
+  // log.debug(`Ending ... ${arguments.callee.name}`);
+  res.json({
+    item: turnModel,
+  }); // new true говорит отдать новую модель, а не старую
 }
 
 async function saveTurn(req, res, next) {
-    // бусы на нитке - функции в Node все работают с req res
-    try {
-        const { gameId } = req.gameInfo;
-        log.debug(`Entering ... ${arguments.callee.name}`);
-        let turn = req.body; // деструктуризатор
-        delete turn._id;
-        const turnModel = new Turn({
-            ...turn,
-            gameId,
-        });
-        // @todo: пересмотреть
-        if (turn.contentType === 'comment') {
-            turnModel.header = 'comment';
-        }
-        await turnModel.save();
-        res.json({
-            // json, render, next - один из трёх завершает обработку
-            item: turnModel,
-        });
-    } catch (error) {
-        next(error);
+  // бусы на нитке - функции в Node все работают с req res
+  try {
+    const { gameId } = req.gameInfo;
+    log.debug(`Entering ... ${arguments.callee.name}`);
+    let turn = req.body; // деструктуризатор
+    delete turn._id;
+    const turnModel = new Turn({
+      ...turn,
+      gameId,
+    });
+    // @todo: пересмотреть
+    if (turn.contentType === 'comment') {
+      turnModel.header = 'comment';
     }
+    await turnModel.save();
+    res.json({
+      // json, render, next - один из трёх завершает обработку
+      item: turnModel,
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function getTurns(req, res) {
-    const { gameId } = req.gameInfo;
-    // log.debug(`Entering ... ${arguments.callee.name}`);
-    const turns = await Turn.find({
-        gameId,
-    });
-    // log.debug(`Ending ... ${arguments.callee.name}`);
-    res.json({
-        items: turns,
-    });
+  const { gameId } = req.gameInfo;
+  // log.debug(`Entering ... ${arguments.callee.name}`);
+  const turns = await Turn.find({
+    gameId,
+  });
+  // log.debug(`Ending ... ${arguments.callee.name}`);
+  res.json({
+    items: turns,
+  });
 }
 
 async function updateCoordinates(req, res) {
-    const { gameId } = req.gameInfo;
-    const time = Date.now();
-    const { turns = [] } = req.body;
-    const items = [];
-    for (let turn of turns) {
-        const { id, x, y, height, width, contentType, scrollPosition } = turn;
+  const { gameId } = req.gameInfo;
+  const time = Date.now();
+  const { turns = [] } = req.body;
+  const items = [];
+  for (let turn of turns) {
+    const { id, x, y, height, width, contentType, scrollPosition } = turn;
 
-        // Turn.findOneAndUpdate({
-        //     _id: id
-        // }, {
-        //     x, y, height, width, contentType, scrollPosition
-        // })
+    // Turn.findOneAndUpdate({
+    //     _id: id
+    // }, {
+    //     x, y, height, width, contentType, scrollPosition
+    // })
 
-        const turnModel = await Turn.findOne({ _id: id, gameId });
-        turnModel.x = x;
-        turnModel.y = y;
-        turnModel.height = height;
-        turnModel.width = width;
-        turnModel.contentType = contentType; // @todo: delete
-        turnModel.scrollPosition = scrollPosition;
+    const turnModel = await Turn.findOne({ _id: id, gameId });
+    turnModel.x = x;
+    turnModel.y = y;
+    turnModel.height = height;
+    turnModel.width = width;
+    turnModel.contentType = contentType; // @todo: delete
+    turnModel.scrollPosition = scrollPosition;
 
-        turnModel.save();
+    turnModel.save();
 
-        items.push({
-            id: turnModel._id,
-        });
-    }
-    // console.log((Date.now() - time) / 1000);
-    res.json({
-        success: true,
-        items,
+    items.push({
+      id: turnModel._id,
     });
+  }
+  // console.log((Date.now() - time) / 1000);
+  res.json({
+    success: true,
+    items,
+  });
 }
 
 module.exports = {
-    saveTurn,
-    getTurns,
-    updateCoordinates,
-    updateTurn,
-    deleteTurn,
+  saveTurn,
+  getTurns,
+  updateCoordinates,
+  updateTurn,
+  deleteTurn,
 };
--- server/scripts/db_dump.js
+++ server/scripts/db_dump.js
@@ -2,32 +2,32 @@ const { resolve } = require('path');
 const fs = require('fs');
 const { exec } = require('child_process');
 require('dotenv').config({
-    path: resolve(__dirname, '../.env'),
+  path: resolve(__dirname, '../.env'),
 });
 
 const dumpsDir = resolve(__dirname, '../dumps');
 const date = new Date();
 let folderName = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(
-    -2
+  -2
 )}-${('0' + date.getDate()).slice(-2)}-`;
 
 const files = fs.readdirSync(dumpsDir);
 for (let i = 1; i < files.length + 2; i++) {
-    if (files.indexOf(`${folderName}${i}`) === -1) {
-        folderName = `${folderName}${i}`;
-        break;
-    }
+  if (files.indexOf(`${folderName}${i}`) === -1) {
+    folderName = `${folderName}${i}`;
+    break;
+  }
 }
 const targetFolder = resolve(dumpsDir, `./${folderName}`);
 
 const command = `mongodump --uri "${process.env.MONGO_URL}" --out ${targetFolder}`;
 
 exec(command, (err, stdout, stderr) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-    }
-    process.exit();
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  }
+  process.exit();
 });
