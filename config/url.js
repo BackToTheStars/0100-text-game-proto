a1387dc5cc2c07c090e886fd4703294b16b75560
naukomania <naukomania@gmail.com>
2022-10-05 17:44:10 +0300

    refactoring and fixes

---
+++ config/url.js
@@ -0,0 +1,7 @@
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3002';
+const STATIC_API_URL = process.env.STATIC_API_URL || 'http://localhost:3003';
+
+module.exports = {
+  CLIENT_URL,
+  STATIC_API_URL
+};
