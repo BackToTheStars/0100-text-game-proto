4071a5a8e0bd74aef5051b97b537aa3d51189a59
naukomania <naukomania@gmail.com>
2024-08-11 23:01:47 +0300

    audio server config fixed

--- config/url.js
+++ config/url.js
@@ -2,7 +2,7 @@ const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3002';
 const API_URL = process.env.API_URL || 'http://localhost:3000';
 const STATIC_API_URL = process.env.STATIC_API_URL || 'http://localhost:3003';
 const STATIC_AUDIO_URL =
-  process.env.NEXT_PUBLIC_STATIC_AUDIO_URL || 'http://localhost:3010';
+  process.env.STATIC_AUDIO_URL || 'http://localhost:3010';
 
 module.exports = {
   CLIENT_URL,
